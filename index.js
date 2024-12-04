ace81702f1f63de3d6eea2a94c63e8431eee2b23
Davi Cruz <cruzdavi001@gmail.com>
2024-12-04 14:10:59 -0300

    Começando

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+const app = express();
+
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/conteudo/index.html");
+});
+
+app.get("/listar", (req, res) => {
+  res.sendFile(__dirname + "/conteudo/listar.html");
+});
+
+app.get("/cadastrar", (req, res) => {
+  res.sendFile(__dirname + "/conteudo/cadastrar.html");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor online em http://localhost:3000");
+});
